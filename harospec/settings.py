3a948632004af5d4cb042ca5f597f672a4de4044
Reid Haefer <reid.haefer@rsginc.com>
2025-01-11 09:43:35 -0800

    da

--- harospec/settings.py
+++ harospec/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'harospec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Ensure this includes the templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
